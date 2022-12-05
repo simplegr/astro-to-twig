617f44ec2d82013441b77d92094c63b0bbee7add
George Giannoulopoulos <vdotoree@yahoo.gr>
2022-12-05 09:49:55 +0200

    Various fixes

--- src/converter.py
+++ src/converter.py
@@ -1,29 +1,49 @@
 import glob, re, os
-# from bs4 import BeautifulSoup
 
 # Configuration
-template = "../components/svg/*.astro"
-template = "../components/test-01/*.astro"
+template = "../components/test-01/my-component.astro"
+convert_html_comments_to_twig = True
+use_only_for_twig_parameters = True
+astro_components_alias = '@components'
+twig_components_alias = '_components'
+
+# Attributes that will be replaced
+common_attributes_replacements = {
+    'classes': 'class',
+}
 
 cwd = os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+####
 
 def split_frontmatter_lines(text):
     lines = text.split('\n')
     return list(filter(lambda x: x != '', lines))
 
+def replace_common_attributes(attributeName):
+    for key, value in common_attributes_replacements.items():
+        attributeName = attributeName.replace(key, value)
+    return attributeName
+
 def extract_frontmatter_components(lines):
     components = []
+    import_template = r'import\s+(\w+)\s+from\s+("%s[^"]+")' % (astro_components_alias)
     for line in lines:
-        result = re.match(r'import\s+(\w+)\s+from\s+("@components[^"]+")', line)
+        result = re.match(import_template, line)
         if result != None:
             file = re.sub(r'(astro)"$', 'twig', result.group(2))
-            file = re.sub(r'^"(@components)', '_components', file)
+            file = re.sub(r'^"(%s)' % (astro_components_alias), twig_components_alias, file)
             compo = {
-                "name": result.group(1),
-                "file": file
+                'name': result.group(1),
+                'file': file
             }
             components.append(compo)
     return components
+
+def convert_comments(body):
+    if convert_html_comments_to_twig:
+        body = re.sub(r'<!--(.*?)-->', r'{# -- \1 -- #}', body)
+    return body
 
 def auto_closing_astro_tag_to_twig_include(match, compo):
     """
@@ -44,7 +64,8 @@ def auto_closing_astro_tag_to_twig_include(match, compo):
     params = ''
     statement = ''
     if match.group(1) is not None:
-        statement = '{%% include "%s" with { %s } %%}' % (compo["file"], '_PARAMS_')
+        params_template = 'with { _PARAMS_ } only' if use_only_for_twig_parameters else 'with { _PARAMS_ }'
+        statement = '{%% include "%s" %s %%}' % (compo["file"], params_template)
     if match.group(2) is not None:
         parts = []
         attributes = re.split(r'([^=]+=".*"+)', match.group(2))
@@ -52,13 +73,16 @@ def auto_closing_astro_tag_to_twig_include(match, compo):
         for attribute in attributes:
             result = re.match(r'^([^=]+)=(".*"+)$', attribute.strip())
             if result is not None:
-                parts.append(': '.join([result.group(1), result.group(2)]))
+                attributeName = replace_common_attributes(result.group(1))
+                parts.append(': '.join([attributeName, result.group(2)]))
         params = ', '.join(parts)
     statement = statement.replace('_PARAMS_', params)
 
     return statement
 
 def convert_body(body, components):
+    # Convert html comments to twig
+    body = convert_comments(body)
     output = []
     for compo in components:
         pattern = rf'(<{compo["name"]})\s*(.*)\s*\/>'
